4afbac1f46af62cc4cf3e1b080c20ec80890e42e
antonio <andrade.antonio@gmail.com>
2021-12-13 18:34:10 +0400

    update

--- app/api.py
+++ app/api.py
@@ -101,7 +101,7 @@ async def add_friend():
 
 @api.route("/ref", methods=["GET"])
 async def ref():
-    data = await request.args()
+    data = await request.args
     url = data.get("url")
     logger.info(url)
     return redirect(location=url, code=301)
