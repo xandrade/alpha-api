2c1735cbf6a4b9f9dc87f22659b1cb4423f8b88b
antonio <andrade.antonio@gmail.com>
2021-12-16 08:51:51 +0400

    update

--- app/api.py
+++ app/api.py
@@ -140,7 +140,7 @@ async def ref():
     """
 
 
-@api.route("/gallery/<int:video_pairs", methods=['GET'])
+@api.route("/gallery/<int:video_pairs>", methods=["GET"])
 async def gallery(video_pairs=3):
 
     _html = f"""
@@ -157,11 +157,46 @@ async def gallery(video_pairs=3):
 				</article>
     """
 
-    videos = ['https://www.youtube.com/watch?v=pAVk0tLJvA0', 'https://www.youtube.com/watch?v=QMyahx3soiM', 'https://www.youtube.com/watch?v=ffC08UqcFw0', 'https://www.youtube.com/watch?v=IeXppHVrsug', 'https://www.youtube.com/watch?v=EvLcyB9VNrU', 'https://www.youtube.com/watch?v=gXRGI0NE-7E', 'https://www.youtube.com/watch?v=wxGyA9mGRK8', 'https://www.youtube.com/watch?v=-fukPEXrQas', 'https://www.youtube.com/watch?v=8fGe2sidmGg', 'https://www.youtube.com/watch?v=1JmBBX8KOOQ', 'https://www.youtube.com/watch?v=kvza7Y2_FNk', 'https://www.youtube.com/watch?v=-xEzBOO337A', 'https://www.youtube.com/watch?v=HtVBWnQNaGE', 'https://www.youtube.com/watch?v=EGGM94l1fKU', 'https://www.youtube.com/watch?v=FB12t1_LHWI', 'https://www.youtube.com/watch?v=r92LBThuBmc', 'https://www.youtube.com/watch?v=7ZEdnyHQ_No', 'https://www.youtube.com/watch?v=ahaI1xRKYYk', 'https://www.youtube.com/watch?v=ERFin10XwDw', 'https://www.youtube.com/watch?v=Te7k0T_hkkI', 'https://www.youtube.com/watch?v=mwthQLYEcNk', 'https://www.youtube.com/watch?v=KmcjB_Fdxm0', 'https://www.youtube.com/watch?v=CTgbZ8tOu2c', 'https://www.youtube.com/watch?v=96r-OdsZcgE', 'https://www.youtube.com/watch?v=3Slh4cICQDs', 'https://www.youtube.com/watch?v=_Lssqh8m7oE', 'https://www.youtube.com/watch?v=-gk1Lc6hE2Y', 'https://www.youtube.com/watch?v=lPRo4B36AaA', 'https://www.youtube.com/watch?v=bdl7GgI7egY', 'https://www.youtube.com/watch?v=n3PUlsd5tlc', 'https://www.youtube.com/watch?v=Wmug-C65tGI', 'https://www.youtube.com/watch?v=PvXZBcLuTPs', 'https://www.youtube.com/watch?v=QMyahx3soiM', 'https://www.youtube.com/watch?v=GnuHsc1S5vY',]
-    random_video = random.choice(videos, k=video_pairs*2)
+    videos = [
+        "https://www.youtube.com/watch?v=pAVk0tLJvA0",
+        "https://www.youtube.com/watch?v=QMyahx3soiM",
+        "https://www.youtube.com/watch?v=ffC08UqcFw0",
+        "https://www.youtube.com/watch?v=IeXppHVrsug",
+        "https://www.youtube.com/watch?v=EvLcyB9VNrU",
+        "https://www.youtube.com/watch?v=gXRGI0NE-7E",
+        "https://www.youtube.com/watch?v=wxGyA9mGRK8",
+        "https://www.youtube.com/watch?v=-fukPEXrQas",
+        "https://www.youtube.com/watch?v=8fGe2sidmGg",
+        "https://www.youtube.com/watch?v=1JmBBX8KOOQ",
+        "https://www.youtube.com/watch?v=kvza7Y2_FNk",
+        "https://www.youtube.com/watch?v=-xEzBOO337A",
+        "https://www.youtube.com/watch?v=HtVBWnQNaGE",
+        "https://www.youtube.com/watch?v=EGGM94l1fKU",
+        "https://www.youtube.com/watch?v=FB12t1_LHWI",
+        "https://www.youtube.com/watch?v=r92LBThuBmc",
+        "https://www.youtube.com/watch?v=7ZEdnyHQ_No",
+        "https://www.youtube.com/watch?v=ahaI1xRKYYk",
+        "https://www.youtube.com/watch?v=ERFin10XwDw",
+        "https://www.youtube.com/watch?v=Te7k0T_hkkI",
+        "https://www.youtube.com/watch?v=mwthQLYEcNk",
+        "https://www.youtube.com/watch?v=KmcjB_Fdxm0",
+        "https://www.youtube.com/watch?v=CTgbZ8tOu2c",
+        "https://www.youtube.com/watch?v=96r-OdsZcgE",
+        "https://www.youtube.com/watch?v=3Slh4cICQDs",
+        "https://www.youtube.com/watch?v=_Lssqh8m7oE",
+        "https://www.youtube.com/watch?v=-gk1Lc6hE2Y",
+        "https://www.youtube.com/watch?v=lPRo4B36AaA",
+        "https://www.youtube.com/watch?v=bdl7GgI7egY",
+        "https://www.youtube.com/watch?v=n3PUlsd5tlc",
+        "https://www.youtube.com/watch?v=Wmug-C65tGI",
+        "https://www.youtube.com/watch?v=PvXZBcLuTPs",
+        "https://www.youtube.com/watch?v=QMyahx3soiM",
+        "https://www.youtube.com/watch?v=GnuHsc1S5vY",
+    ]
+    random_video = random.choice(videos, k=video_pairs * 2)
 
     for i in range(video_pairs):
-        left, right = random_video[i*2], random_video[i*2+1]
+        left, right = random_video[i * 2], random_video[i * 2 + 1]
         _html = f"""
                 <article class="from-left">
 					<iframe width="100%" height="350" src="https://www.youtube.com/embed/{left}?controls=0"
