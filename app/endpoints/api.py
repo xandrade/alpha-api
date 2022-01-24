0d4e2aaf120e8da2fc8bbf78a0b8f2a9db08abc9
xandrade <andrade.antonio@gmail.com>
2022-01-24 14:26:34 +0400

    update

--- app/endpoints/api.py
+++ app/endpoints/api.py
@@ -223,27 +223,23 @@ async def ref():
                 <p><a href="{url}">{url}</a></p>
             </div>
             <div style="width: 100px; justify-content: center; display: inline-flex;">
-                <svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-                    <rect x="20" y="50" width="4" height="10" fill="#000">
-                    <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0" begin="0" dur="0.6s" repeatCount="indefinite"></animateTransform>
-                    </rect>
-                    <rect x="30" y="50" width="4" height="10" fill="#000">
-                    <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0" begin="0.2s" dur="0.6s" repeatCount="indefinite"></animateTransform>
-                    </rect>
-                    <rect x="40" y="50" width="4" height="10" fill="#000">
-                    <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0" begin="0.4s" dur="0.6s" repeatCount="indefinite"></animateTransform>
-                    </rect>
-                    <rect x="50" y="50" width="4" height="10" fill="#000">
-                    <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0" begin="0.6s" dur="0.6s" repeatCount="indefinite"></animateTransform>
-                    </rect>
-                    <rect x="60" y="50" width="4" height="10" fill="#000">
-                    <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0" begin="0.8s" dur="0.6s" repeatCount="indefinite"></animateTransform>
-                    </rect>
-                    <rect x="70" y="50" width="4" height="10" fill="#000">
-                    <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0" begin="1.0s" dur="0.6s" repeatCount="indefinite"></animateTransform>
-
-
-                </svg>
+            <svg version="1.1" id="L4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+                <circle fill="#000" stroke="none" cx="6" cy="50" r="6">
+                    <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1"></animate>    
+                </circle>
+                <circle fill="#000" stroke="none" cx="26" cy="50" r="6">
+                    <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.2"></animate>       
+                </circle>
+                <circle fill="#000" stroke="none" cx="46" cy="50" r="6">
+                    <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.3"></animate>     
+                </circle>
+                <circle fill="#000" stroke="none" cx="66" cy="50" r="6">
+                    <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.4"></animate>     
+                </circle>
+                <circle fill="#000" stroke="none" cx="86" cy="50" r="6">
+                    <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.5"></animate>     
+                </circle>
+            </svg>
             </div>
 		</div>
 	</section>
